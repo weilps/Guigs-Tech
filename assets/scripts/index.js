cbd52daece144aabc44085f9bdcfcbf2961b1c42
weilps <guillaume.gaudin75@gmail.com>
2023-01-29 17:53:33 +1100

    v0.5
    update HMTL / SASS
    start JS

---
+++ assets/scripts/index.js
@@ -0,0 +1,26 @@
+//SASS colors
+const sassColor1 = "rgb(249, 249, 249)";
+const sassColor2 = "rgb(0 51 102)";
+const sassColor3 = "rgb(0 51 102)";
+const sassColor3t = "rgba(0, 51, 102, 0.5)";
+const sassColor4 = "rgb(20, 20, 20)";
+const sassColor5 = "rgb(161, 198, 236)";
+
+// Scroll object from Locomotive
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+  tablet: { smooth: true },
+  smartphone: { smooth: true },
+});
+
+// Body color
+scroll.on("scroll", () => {
+  if (document.querySelector("#color.is-inview")) {
+    document.body.style.background = sassColor3;
+    document.body.style.color = sassColor1;
+  } else {
+    document.body.style.background = sassColor1;
+    document.body.style.color = sassColor3;
+  }
+});
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,51 +1,48 @@
-// Script pour que la nav defile
+// ******* Script pour que la nav defile *******
 
 // Récupération de l'élément header
-const header = document.querySelector('header');
+const header = document.querySelector("header");
 
 // Ajout d'un écouteur d'événement pour l'événement scroll
-window.addEventListener('scroll', function() {
+window.addEventListener("scroll", function () {
   // Récupération de la position verticale de l'utilisateur
   const scroll = window.scrollY;
 
   // Si la position verticale de l'utilisateur est supérieure à 100px
   if (scroll > 10) {
     // Ajout de la classe 'sticky' à l'élément header
-    header.classList.add('sticky');
+    header.classList.add("sticky");
   } else {
     // Suppression de la classe 'sticky' de l'élément header
-    header.classList.remove('sticky');
+    header.classList.remove("sticky");
   }
 });
 
+// ******* *******
 
 // Script pour afficher le burger bun menu en mobile
 
-const menuButton = document.querySelector('.menu-button');
-const navM = document.querySelector('.menu');
+const menuButton = document.querySelector(".menu-button");
+const navM = document.querySelector(".menu");
 
-menuButton.addEventListener('click', () => {
-  navM.classList.toggle('show');
-  if(navM.classList.contains('show')) {
-    navMobile.style.display = 'flex';
-  setTimeout(() => {
-    navM.classList.add('show-transition');
-}, 20);
-} else {
-
-navM.classList.remove('show-transition');
-navM.addEventListener("transitionend", () => {
-    navM.style.display = 'none';
+menuButton.addEventListener("click", () => {
+  navM.classList.toggle("show");
+  if (navM.classList.contains("show")) {
+    navMobile.style.display = "flex";
+    setTimeout(() => {
+      navM.classList.add("show-transition");
+    }, 20);
+  } else {
+    navM.classList.remove("show-transition");
+    navM.addEventListener("transitionend", () => {
+      navM.style.display = "none";
+    });
+  }
 });
-
-}
-});
-
-
 
 // Script pour montrer les alertes du form quand on clique sur le submit
 
-const container = document.querySelector('.alert-duration');
+const container = document.querySelector(".alert-duration");
 // const button = container.querySelector('.submit-bt');
 // const alert2 = container.querySelector('sl-alert');
 const isSuccess = "<?php echo $success; ?>";
@@ -56,11 +53,11 @@ if (isSuccess == false) {
   document.getElementById("error-message").style.display = "block";
 }
 
-button.addEventListener('click', () => alert2.show());
+// button.addEventListener("click", () => alert2.show());
 
 // Script pour fermer les alertes du form
 
-const alert1 = document.querySelector('.alert-closable');
-  alert1.addEventListener('sl-after-hide', () => {
-    setTimeout(() => (alert1.open = true), 2000);
-  });
+// const alert1 = document.querySelector(".alert-closable");
+// alert1.addEventListener("sl-after-hide", () => {
+//   setTimeout(() => (alert1.open = true), 2000);
+// });
